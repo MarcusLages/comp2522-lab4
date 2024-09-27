320badbb37d39bf8de7ca08b6d972f43929213ee
MarcusLages <marcusvlages@gmail.com>
2024-09-27 12:31:35 -0700

    Implemented Biography.java

--- src/code/ca/bcit/comp2522/lab4/BAM/Biography.java
+++ src/code/ca/bcit/comp2522/lab4/BAM/Biography.java
@@ -1,4 +1,45 @@
 package ca.bcit.comp2522.lab4.BAM;
 
-public class Biography {
+public class Biography extends Book {
+
+    public final Person subject;
+
+    public Biography(final String title, final int yearPublished, final Author author, final Person subject) {
+        super(title, yearPublished, author);
+        validateSubject(subject);
+
+        this.subject = subject;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if(obj == null) {
+            return false;
+        }
+
+        if(obj == this) {
+            return true;
+        }
+
+        if(!(obj instanceof Biography)) {
+            return false;
+        }
+
+        final Biography bioCompare;
+        final boolean equal;
+        bioCompare = (Biography) obj;
+        equal = (this.subject.equals(bioCompare));
+        return equal;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(subject.name);
+    }
+
+    private static void validateSubject(final Person subject) {
+        if (subject == null) {
+            throw new IllegalArgumentException("Null biography subject is not valid.");
+        }
+    }
 }
