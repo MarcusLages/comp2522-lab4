9b114675f2093a54aa4bd98a679e7dccbaf9f092
MarcusLages <marcusvlages@gmail.com>
2024-09-27 13:18:49 -0700

    Documented Autobiography.java

--- src/code/ca/bcit/comp2522/lab4/BAM/Autobiography.java
+++ src/code/ca/bcit/comp2522/lab4/BAM/Autobiography.java
@@ -1,7 +1,21 @@
 package ca.bcit.comp2522.lab4.BAM;
 
+/**
+ * Class that represents an autobiography book and its information (title, year
+ *  * published and author).
+ *
+ * @author Ben, Marcus, Andre
+ * @version 1.0
+ */
 public class Autobiography extends Biography {
 
+    /**
+     * Creates an Autobiography object based on the given information.
+     *
+     * @param title         title of the autobiography
+     * @param yearPublished year the autobbiography was published
+     * @param author        author and subject of the autobiography
+     */
     public Autobiography(final String title,
                          final int yearPublished,
                          final Author author) {
