800a12300d89fa4ffa4b2afe1b6131255dc3f73c
MarcusLages <marcusvlages@gmail.com>
2024-09-27 12:45:14 -0700

    Implemented Autobiography.java

--- src/code/ca/bcit/comp2522/lab4/BAM/Autobiography.java
+++ src/code/ca/bcit/comp2522/lab4/BAM/Autobiography.java
@@ -1,4 +1,11 @@
 package ca.bcit.comp2522.lab4.BAM;
 
-public class Autobiography {
+public class Autobiography extends Biography {
+
+    public Autobiography(final String title,
+                         final int yearPublished,
+                         final Author author) {
+        super(title, yearPublished, author, author.person);
+    }
+
 }
